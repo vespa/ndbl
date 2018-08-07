3063fbed3a34ec0f054c3fdcc136f0d8c281e2d9
Unknown <danilo.vespa@dafiti.com.br>
2018-08-07 17:19:08 -0300

    Removing comments

--- src/containers/Form/index.jsx
+++ src/containers/Form/index.jsx
@@ -1,56 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import { test } from './redux';
-/* eslint-disable */
+import PropTypes from 'prop-types';
 
-// const Form = ({ props }) => {
-//   console.log(props);
-//   return (
-//     <div>
-//       oi
-//     </div>);
-// };
-
-// const mapStateToProps = (state) => {
-//   return {
-//     test: state.form.test,
-//   };
-// };
-
-// Form.propTypes = {
-//   props: PropTypes.shape({}).isRequired,
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     fetchData: url => dispatch(test(url)),
-//   };
-// };
-
-// class Form extends React.Component {
-//   constructor(args) {
-//     super(args);
-//     console.log(args);
-//   }
-//   render() {
-//     return (<div> {this.props.test.toString()} </div>)
-//   }
-// }
-
-const Form = (test) => {
-  console.log(test);
+const Form = ({ test }) => {
   return (
     <div>
-      oi
+      {test.toString()}
     </div>);
 };
 
-
-// Form.propTypes = {
-//   props: PropTypes.shape({}).isRequired,
-// };
-
+Form.propTypes = {
+  test: PropTypes.bool.isRequired,
+};
 
 const mapStateToProps = (state) => {
   return {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -8,30 +8,16 @@ const middlewares = [
 ];
 let devToolsExtension = f => f;
 
-/* istanbul ignore if  */
 if (process.env.NODE_ENV === 'development') {
-//  const { createLogger } = require('redux-logger');
-// const logger = createLogger({ collapsed: true });
-//  middlewares.push(logger);
   if (window.devToolsExtension) {
     devToolsExtension = window.devToolsExtension();
   }
 }
 
-// console.log(rootReducer);
 export default function configReduxStore(initialState) {
   const store = createStore(combinedReducers, initialState, compose(
     applyMiddleware(...middlewares),
     devToolsExtension,
   ));
-
-  /* istanbul ignore if  */
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('./rootReducer', () => {
-  //     const nextRootReducer = require('./rootReducer').default; // eslint-disable-line
-  //     store.replaceReducer(nextRootReducer);
-  //   });
-  // }
   return store;
 }
