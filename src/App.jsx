1b09cb5ace09f54c757a3dec0e7df072e9b35742
Vespa <danilo.vespa@dafiti.com.br>
2018-08-07 14:41:05 -0300

    adding airbnb eslint validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
+  constructor(args) {
+    super(args);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to React
+          </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>
+            src/App.js
+          </code>
+          and save to reload.
         </p>
       </div>
     );
